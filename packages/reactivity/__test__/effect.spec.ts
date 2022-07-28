cf544b563fbb0b2105e01c57092987738eb6388b
a1344359061@gmail.com <erde.zh@dtyunxi.com>
2022-07-28 20:30:37 +0800

    初始化reactive

---
+++ packages/reactivity/__test__/effect.spec.ts
@@ -0,0 +1,14 @@
+describe("effect", () => {
+  it.skip("happy path", () => {
+    const user = reactive({
+      age: 10,
+    });
+    let newAge;
+    effect(() => {
+      newAge = user.age + 1;
+    });
+    expect(newAge).toBe(11);
+    user.age++;
+    expect(newAge).toBe(12);
+  });
+});
---
+++ packages/reactivity/src/reactive.ts
@@ -0,0 +1,8 @@
+export function reactive(raw) {
+  return new Proxy(raw, {
+    get(target, key) {
+      const res = Reflect.get(target, key);
+      return res;
+    },
+  });
+}
